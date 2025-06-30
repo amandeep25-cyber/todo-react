1f14dc9371abf4c2a862adeccacb2cb377af028e
amandeep25 <st.amandeep25@gmail.com>
2025-07-01 00:15:12 +0530

    Adding task creating functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./App.css"
-import TodoName from "./components/TodoName"
-import TodoInput from "./components/TodoInput"
-import TodoList from "./components/TodoList"
-import { useState } from "react"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import TodoName from "./components/TodoName";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import { useState } from "react";
 
 function App() {
-  let [ListItems,setListItems] = useState([
+  let [ListItems, setListItems] = useState([
     {
-      task:"hii i am here",
-      date:"10/12/2025"
+      task: "hii i am here",
+      date: "10/12/2025",
     },
     {
-      task:"hdsf",
-      date:"10/12/2025"
+      task: "hdsf",
+      date: "10/12/2025",
+    },
+  ]);
+
+  const createTask = (taskVal, dateVal) => {
+    if (taskVal && dateVal) {
+      const newList = {
+        task: taskVal,
+        date: dateVal,
+      };
+      const newListItems = [...ListItems, newList];
+      setListItems(newListItems);
     }
-  ])
+  };
 
   return (
     <>
-      
       <div className="container">
-          <TodoName />
-          <TodoInput />
+        <TodoName />
+        <TodoInput createTask={createTask} />
       </div>
 
       <div className="container">
-            <TodoList listItems={ListItems}/>
+        <TodoList listItems={ListItems} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,12 +1,37 @@
-import "./TodoInput.css"
+import "./TodoInput.css";
+import { useState } from "react";
 
-const TodoInput = () => {
+const TodoInput = ({ createTask }) => {
+  let [taskVal, setTaskVal] = useState();
+  let [dateVal, setdateVal] = useState();
+
+  const takeInputTask = (event) => {
+    setTaskVal(event.target.value);
+  };
+
+  const takeInputDate = (e) => {
+    setdateVal(e.target.value);
+  };
+
   return (
     <>
       <div className="input-container">
-            <input type="text" placeholder="Enter your task" className="input-tag"/>
-            <input type="date" className="input-tag"/>
-            <button className="add-btn">Add</button>
+        <input
+          type="text"
+          placeholder="Enter your task"
+          className="input-tag"
+          onChange={takeInputTask}
+          value={taskVal}
+        />
+        <input
+          type="date"
+          className="input-tag"
+          onChange={takeInputDate}
+          value={dateVal}
+        />
+        <button className="add-btn" onClick={()=>createTask(taskVal,dateVal)}>
+          Add
+        </button>
       </div>
     </>
   );
