498ee9d7a2eb4d66004e5271248ce2746e938dc7
amandeep25 <st.amandeep25@gmail.com>
2025-06-30 23:34:15 +0530

    UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,31 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
 import TodoName from "./components/TodoName"
 import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+import { useState } from "react"
+
 function App() {
-  
+  let [ListItems,setListItems] = useState([
+    {
+      task:"hii i am here",
+      date:"10/12/2025"
+    },
+    {
+      task:"hdsf",
+      date:"10/12/2025"
+    }
+  ])
 
   return (
     <>
+      
       <div className="container">
           <TodoName />
           <TodoInput />
+      </div>
+
+      <div className="container">
+            <TodoList listItems={ListItems}/>
       </div>
     </>
   )
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,1 +1,21 @@
+import "./TodoList.css"
 
+const TodoList = ({ listItems }) => {
+  return (
+    <>
+    {listItems.map(listItem => {
+        return (
+        <li className="input-container">
+        <p className="task">{listItem.task}</p>
+        <p className="task">{listItem.date}</p>
+        <button className="remove-btn">Delete</button>
+      </li>
+        
+      )
+    })}
+      
+    </>
+  );
+};
+
+export default TodoList;
